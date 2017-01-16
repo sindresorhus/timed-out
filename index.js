71f638eb4d85c8b1e03ae60c8d58bd3b989859c3
Vsevolod Strukchinsky <floatdrop@gmail.com>
2017-01-16 18:57:45 +0500

    Revert "simplify code (move out socket timeout part)"
    This reverts commit 42f6998828a04d1cf10c6929cb51317df39bbf05.

--- index.js
+++ index.js
@@ -17,33 +17,37 @@ module.exports = function (req, time) {
 		}, delays.connect);
 	}
 
-	if (delays.socket !== undefined) {
-		// Abort the request if there is no activity on the socket for more
-		// than `delays.socket` milliseconds.
-		req.setTimeout(delays.socket, function socketTimeoutHandler() {
-			req.abort();
-			var e = new Error('Socket timed out on request' + host);
-			e.code = 'ESOCKETTIMEDOUT';
-			req.emit('error', e);
-		});
-	}
-
 	// Clear the connection timeout timer once a socket is assigned to the
 	// request and is connected.
 	req.on('socket', function assign(socket) {
 		// Socket may come from Agent pool and may be already connected.
 		if (!(socket.connecting || socket._connecting)) {
-			clear();
+			connect();
 			return;
 		}
 
-		socket.once('connect', clear);
+		socket.once('connect', connect);
 	});
 
 	function clear() {
 		if (req.timeoutTimer) {
 			clearTimeout(req.timeoutTimer);
 			req.timeoutTimer = null;
+		}
+	}
+
+	function connect() {
+		clear();
+
+		if (delays.socket !== undefined) {
+			// Abort the request if there is no activity on the socket for more
+			// than `delays.socket` milliseconds.
+			req.setTimeout(delays.socket, function socketTimeoutHandler() {
+				req.abort();
+				var e = new Error('Socket timed out on request' + host);
+				e.code = 'ESOCKETTIMEDOUT';
+				req.emit('error', e);
+			});
 		}
 	}
 
